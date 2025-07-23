9e63c7f2423eaac75ef068e69b2dd57eaf6e8e27
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-07-23 12:48:51 +0100

    multa auto estrada

--- main.py
+++ main.py
@@ -16,4 +16,12 @@ def multa_fora_da_localidade(velocidade):
     else:
         return 0
 
-
+def multa_auto_estrada(velocidade):
+    if velocidade > 120 and velocidade <= 150:
+        return 60
+    elif velocidade > 150 and velocidade <= 175:
+        return 120
+    elif velocidade > 175:
+        return 360
+    else:
+        return 0
