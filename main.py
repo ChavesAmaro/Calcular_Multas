8782730c55528dda52e622b9132c3ab1df130246
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-07-23 13:33:02 +0100

    função main adicionada

--- main.py
+++ main.py
@@ -33,4 +33,38 @@ def menu():
     print("3 - Auto Estrada")
     print("0 - Sair")
     
+def main():
+    loop = True
+    while loop == True:
+        menu()
+        escolha = input("Escolha o tipo de estrada (0 a 3):\n")
+        
+        if escolha == "0":
+            print("A encerrar o programa...")
+            loop = False
+            continue
+        velocidade = int(input("Introduza a velocidade do veículo (km/h)"))
+        
+        if escolha == "1":
+            multa = multa_localidade(velocidade)
+            estrada = "Numa Localidade"
+        elif escolha == "2":
+            multa = multa_fora_da_localidade(velocidade)
+            estrada = "Fora da Localidade"
+        elif escolha == "3":
+            multa = multa_auto_estrada(velocidade)
+            estrada = "Numa Auto Estrada"
+        else:
+            print("Erro! Opção inválida escolha um número de 0 a 3")
+            continue
+            
+        print(f"O veículo seguia a {velocidade} km/h {estrada}.")
+        
+        if multa == 0:
+            print("O condutor não tem de pagar multa.")
+        else:
+            print(f"O condutor tem de pagar uma multa de {multa}€")
 
+
+if __name__ == "__main__":
+    main()
